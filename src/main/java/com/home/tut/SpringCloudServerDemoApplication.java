f9feb41c83d4aa4ac89ce89c46b6887e779122f1
Zoheb Syed <zoheb.syed@outlook.com>
2021-07-11 16:54:28 +0530

    Spring Cloud Config Server Set-Up

---
+++ src/main/java/com/home/tut/SpringCloudServerDemoApplication.java
@@ -0,0 +1,15 @@
+package com.home.tut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/tut/SpringCloudServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.tut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
